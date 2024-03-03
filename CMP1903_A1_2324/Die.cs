21fef52a493abdb5adf79b707d618226ee83af3e
josh8128 <joshhh8128@gmail.com>
2024-03-03 17:57:23 +0000

    Update Die.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -6,18 +6,41 @@ using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
 {
+    //Defines class "die" which represents a single die
     internal class Die
     {
         /*
          * The Die class should contain one property to hold the current die value,
          * and one method that rolls the die, returns and integer and takes no parameters.
          */
+        //Declares an object "RandomNumber" which will be used to generate a random number
+        private RandomNumber randomNumber;
+        //Declares an integer to hold the current value on the die
+        private int dieValue;
 
         //Property
 
 
         //Method
+    //Constructor for die class
+        public Die()
+        {
+            randomNumber = new RandomNumber();
+            Roll(); // Initialises the die with a random value
+        }
 
+    //Property to hold the die's current value
+        public int DieValue
+        {
+            get { return dieValue; }
+            private set { dieValue = value; }
+        }
 
+    //Method to roll the die, works using the random number generator
+         int Roll()
+        {
+            dieValue = randomNumber.Next(1, 7); 
+            return dieValue;
+        }
     }
 }
